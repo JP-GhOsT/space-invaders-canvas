ac0c83f4a4fced9def62a48e374d63d13d853c5f
JP-GhOsT <jpzmatsui23@gmail.com>
2026-02-11 21:37:19 -0300

    feat: estrutura inicial do jogo Space Invaders com nave e sistema de disparo

---
+++ README.md
@@ -0,0 +1,81 @@
+👾 Space Invaders - HTML Canvas 👾
+
+🚧 Projeto em desenvolvimento  
+Sistema de inimigos em fase de implementação.
+
+Projeto desenvolvido para consolidar conceitos de renderização gráfica com HTML5 Canvas e lógica de jogos utilizando JavaScript, baseado em uma aula prática.
+
+📌 Sobre o Projeto
+
+Este projeto é uma versão simplificada do clássico jogo de Atari Space Invaders.
+O jogador controla uma nave que se move horizontalmente e dispara projéteis.
+
+O principal foco deste projeto foi compreender o funcionamento do Canvas e a implementação da lógica de jogos em JavaScript, estruturando o jogo sem o uso de uma engine.
+
+🛠️ Tecnologias Utilizadas
+
+HTML5
+
+CSS3
+
+JavaScript
+
+API Canvas
+
+🎮 Controles
+
+⬅️ A → mover para esquerda
+
+➡️ D → mover para direita
+
+Espaço → atirar
+
+📂 Estrutura do Projeto
+📁 aula-space-invaders
+ ├── index.html
+ ├── style.css
+ ├── README.md
+ └── src
+     ├── index.js
+     ├── classes
+     │    ├── Player.js
+     │    └── Projectile.js
+     ├── utils
+     │    └── constants.js
+     └── assets
+          ├── audios
+          └── images
+
+▶️ Como Executar
+
+Basta abrir o arquivo index.html no navegador.
+
+Se estiver usando VS Code, pode usar a extensão Live Server para facilitar.
+
+📚 O que Aprendi
+
+Criar e utilizar o elemento `canvas`
+
+Desenhar elementos na tela com JavaScript
+
+Criar um loop de animação
+
+Detectar teclas pressionadas
+
+Fazer uma lógica simples de colisão
+
+## 🚀 Próximas Implementações
+
+- Sistema completo de inimigos
+- Detecção de colisão entre projéteis e inimigos
+- Sistema de pontuação
+- Tela de Game Over
+- Versão 2.0 com artes autorais
+- Novas mecânicas de gameplay
+- Reformulação visual e sonora
+
+
+
+🎯 Observação
+
+Este projeto foi feito apenas para fins de aprendizado e prática.
---
+++ src/classes/Player.js
@@ -0,0 +1,103 @@
+import { PATH_SPACESHIP_IMAGE,
+            PATH_ENGINE_IMAGE,
+            PATH_ENGINE_SPRITES,
+            INICIAL_FRAMES
+ } from "../utils/constants.js";
+ import Projectile from "./Projectile.js";
+
+class Player{
+    constructor(canvasWidth, canvasHeight){
+        this.width = 48 * 2;
+        this.height = 48 * 2;
+        this.velocity = 6;
+
+
+        this.position = {
+           x: canvasWidth/2 - this.width/2,
+           y: canvasHeight -this.height - 30,
+        };
+
+        this.image = this.getImage(PATH_SPACESHIP_IMAGE);
+        this.engineimage = this.getImage(PATH_ENGINE_IMAGE);
+        this.engineSprites = this.getImage(PATH_ENGINE_SPRITES);
+
+        this.sx = 0;
+        this.framesCounter = INICIAL_FRAMES;
+    }
+
+    getImage(path){
+        const image = new Image();
+        image.src = path;
+        return image;
+    }
+
+    moveLeft(){
+        this.position.x -= this.velocity;
+    }
+
+
+    moveRight(){
+        this.position.x += this.velocity;
+    }
+
+
+    draw(ctx){
+        ctx.drawImage(
+            this.image, 
+            this.position.x, 
+            this.position.y, 
+            this.width, 
+            this.height
+        );
+
+        ctx.drawImage(
+            this.engineSprites, 
+            this.sx,
+            0, 
+            48,
+            48,
+            this.position.x, 
+            this.position.y + 10, 
+            this.width, 
+            this.height
+        );
+
+        ctx.drawImage(
+            this.engineimage, 
+            this.position.x, 
+            this.position.y + 8, 
+            this.width, 
+            this.height
+        );
+
+        this.update();
+    }
+
+    update(){
+        if(this.framesCounter === 0){
+            if(this.sx === 48 * 3){
+                this.sx = 0;
+            } else {
+                this.sx += 48;
+            }
+
+            this.framesCounter = INICIAL_FRAMES;
+        }
+
+        this.framesCounter--;
+    }
+
+    shoot(projectiles) {
+        const bullet = new Projectile(
+            {
+                x: this.position.x + this.width/2 - 1,
+                y: this.position.y + 2
+            },
+            -10
+        );
+
+        projectiles.push(bullet);
+    }
+}
+
+export default Player;
